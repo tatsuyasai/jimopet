f58fb8937ccd33d8c5fb8e5aada8ac8e606195a9
tatsuya <ta2831jo07@gmail.com>
2020-05-08 11:18:13 +0000

    loginページまで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   add_flash_types :success, :info, :warning, :danger
+  
+  helper_method :current_user, :logged_in? #ApplicationHelperでも使える
+  
+  def current_user　#ログインしているユーザーを返す
+    @current_user ||= User.find_by(id: session[:user_id]) #とにかく省略形
+  end
+  
+  def logged_in? #ログインしているか確認
+    !current_user.nil?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email]) #メールアドレスを取得し、一致するユーザーがいるか検索
+    if user && user.authenticate(params[:session][:password]) #該当のメールアドレスをもつuserが存在している。かつuserのパスワードが正しい
+      log_in user　#private参照
+   　  redirect_to root_path, success: "ログインしました"
+    else
+       flash.now[:danger] = "ログインに失敗しました"
+       render :new
+    end
+  end
+    
+  private
+  def log_in(user)
+      session[:user_id] = user.id #sessionで暗号化して保存
+  end
 end
