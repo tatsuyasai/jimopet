5b4cdd0f00bae4780a837d56b7502fbf8b0fb02c
tatsuya <ta2831jo07@gmail.com>
2020-05-12 14:12:35 +0000

    topic前

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new' 
   get 'pages/index'
+  get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index' #トップページ
   
